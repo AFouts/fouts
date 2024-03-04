c3101167e9cc9152b77d1b87362408363cfbc054
AFouts <adrien_foutelet@brown.edu>
2024-03-04 01:41:53 -0500

    Corrections

--- R/my_plottheme.r
+++ R/my_plottheme.r
@@ -8,8 +8,6 @@
 #' @examples
 #' my_plottheme(plot)
 my_plottheme <- function(plot_in) {
-    devtools::add_package("ggplot2")  
-    devtools::add_package("ggthemes")  
     usethis::use_package("ggplot2")
     usethis::use_package("ggthemes")
     plot_out <- plot_in + theme_economist() + theme(axis.line.x.bottom=element_line(color="blue"), plot.title = element_text(size = rel(2)), plot.background = element_rect(fill = "pink"))
